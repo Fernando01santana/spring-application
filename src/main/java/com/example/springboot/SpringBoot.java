1fdaeececa9cc135f02fb2c5531a2e8dd5a94f8f
Fernando01santana <cool.fernando23@gmail.com>
2024-02-25 23:01:36 -0300

    first commit

---
+++ src/main/java/com/example/springboot/SpringBoot.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot.class, args);
+	}
+
+}
