bff8ce27f73aa60b9122364e0372430a18a84006
Fernando01santana <cool.fernando23@gmail.com>
2024-02-26 11:37:48 -0300

    feat: create method delete

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,12 +45,11 @@ public class ProductController {
         if (searchProduct.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
-
         return ResponseEntity.status(HttpStatus.OK).body(searchProduct.get());
     }
 
-    @PutMapping("products/{id}")
-    public ResponseEntity<Object> putMethodName(@PathVariable(value = "id") UUID id,
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
             @RequestBody @Valid ProductRecordDto productRecordDto) {
         Optional<ProductModel> searchProduct = productRepository.findById(id);
         if (searchProduct.isEmpty()) {
@@ -61,4 +61,14 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
     }
 
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> productExists = productRepository.findById(id);
+        if (productExists.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product does not exist");
+        }
+
+        productRepository.delete(productExists.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully");
+    }
 }
